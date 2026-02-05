10367fc42898fb663092780348e36d86b556d409
gitcoder-amit <yadavamit14801@gmail.com>
2026-02-06 00:13:22 +0530

    integrated groq and LLM model

---
+++ main.py
@@ -0,0 +1,36 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Read the variable
+api_key = os.getenv("API_KEY")
+
+
+def generateRecipe(items):
+    prompt = f"""
+          You are a Recipe suggester assistant. The user will provide a list of ingredients they have on hand, and you will suggest one or more recipe that can be made using those ingredients.
+          {items}
+    """
+
+    client = Groq(
+        api_key = os.getenv("GROQ_API_KEY"),
+    )
+
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages = [
+            {'role': 'user',
+            'content':prompt}
+        ],
+        temperature=1
+    )    
+
+
+    print(completion.choices[0].message.content)
+
+
+items = "Chicken, Rice, Broccoli, Garlic, Soy Sauce"
+generateRecipe(items)
